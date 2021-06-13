82a8d9ebb0bf01108bce0739f79c3fa28a4780b4
jastin250 <85784673+jastin250@users.noreply.github.com>
2021-06-13 18:52:24 +0530

    Add files via upload

---
+++ Daily challenge 2.py
@@ -0,0 +1,51 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print ("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+Hour = "thirty"
+print(Hour)
+Days= "Thirty days"
+print(Days[0])
+challenge="I will win"
+print(challenge[7:10])
+challenge="I will win"
+print(len(challenge))
+challenge="I will win"
+print(challenge.lower())
+a="30 days"
+b="30 hours"
+c=a+b
+print(c)
+a="30 days"
+b="30 hours"
+c=a + " " + b
+print(c)
+text="Thirty days Thirty hours"
+x=text.casefold()
+print(x)
+x=text.capitalize()
+print(x)
+x=text.find("T")
+print(x)
+x=text.find("r")
+print(x)
+x=text.isalpha()
+print(x)
+a="Pythonbootcamp"
+b=a.isalpha()
+print(b)
+a="my age is 10"
+b=a.isalnum()
+print(b)
+#isalnum()
+d="Rating4"
+e=d.isalnum()
+print(e)
+
+
