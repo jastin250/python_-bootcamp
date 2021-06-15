025226e4b518e32b17ee530463826bc1365e9ed9
jastin250 <85784673+jastin250@users.noreply.github.com>
2021-06-15 10:54:44 +0530

    Add files via upload

---
+++ daily challenge 4.py
@@ -0,0 +1,18 @@
+#creating three variable
+a=b=c=200 
+#divide a by 10 
+print("divide a by 10",a/10) 
+#multiply b by 50 
+print("multiply b by 50",b*50) 
+#add c by 60 
+print("add c by 60",c+60)  
+
+#creating string character 
+string_var='dohle' 
+print(string_var.replace("h","G")) 
+
+#convert the datatypes 
+a=12 
+b=12.4 
+print(float(a)) 
+print(int(b))
